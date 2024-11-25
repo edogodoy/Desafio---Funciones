df2cc643b44927d57e2a42940c22b6eedc87baf6
DragoBlanco <dogogrey@gmail.com>
2024-11-24 22:48:14 -0300

    ejercicio 2 logrado

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+const pintar = (elemento, color) => elemento.style.backgroundColor = color;
+
+const ele = document.getElementById("ele1");
+
+ele.addEventListener("click", () => pintar(ele, 'yellow'));
